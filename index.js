163d27d1bf6ffbe9f72556060b39c504251b37ac
rajashekharms369 <rajmkumar369@gmail.com>
2022-06-17 12:44:33 +0530

    17_06_22_nav_foooter_functionality_update

---
+++ index.js
@@ -0,0 +1,39 @@
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+
+  }
+
+
+  // Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+// setInterval(function(){alert("Hello")},3000);
